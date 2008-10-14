59a6a5188949a91a2024020eff1e32d8a8341161
Erwin Waterlander <waterlan@xs4all.nl>
2008-10-14 16:21:24 +0000

    convert src/*.mk files

--- wcd/misc/unix2dos.mk
+++ wcd/misc/unix2dos.mk
@@ -7,7 +7,7 @@ DOSVERSION=wcd${VERSION_SHORT}
 
 unix2dos:
 	cd ../.. ; cp -Rp ${UNIXVERSION} ${DOSVERSION}
-	cd ../../${DOSVERSION}/src ; unix2dos *.h *.c Makefile
+	cd ../../${DOSVERSION}/src ; unix2dos *.h *.c Makefile *.mk
 	cd ../../${DOSVERSION}/src/c3po ; unix2dos *
 	cd ../../${DOSVERSION}/src/patch ; unix2dos *.*
 	cd ../../${DOSVERSION}/src/po ; unix2dos *.po *.pot
