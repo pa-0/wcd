f673de9ed988dcae8cfd3b0f0e6be28cf9c56d1c
Erwin Waterlander <waterlan@xs4all.nl>
2009-07-04 06:36:29 +0000

    5.0.3

--- wcd/src/wcd.h
+++ wcd/src/wcd.h
@@ -80,8 +80,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #  define OP_DIR "."
 #endif /* ?MSDOS|VMS */
 
-#define VERSION      "5.0.3-beta2"
-#define VERSION_DATE "Jun 24 2009"
+#define VERSION      "5.0.3"
+#define VERSION_DATE "Jul 4 2009"
 
 
 /* Function prototypes */
