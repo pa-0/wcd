7205decf042b3e5aef8560163553a08bf9d7db78
Erwin Waterlander <waterlan@xs4all.nl>
2009-09-01 20:04:52 +0000

    Add missing <string.h> include.

--- wcd/src/wcddir.c
+++ wcd/src/wcddir.c
@@ -42,6 +42,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "dosdir.h"
 #include "wcddir.h"
 #include "match.h"
+#include <string.h>
 
 #if (defined(WIN32) && defined(WCD_UNICODE))
 #include <wchar.h>
