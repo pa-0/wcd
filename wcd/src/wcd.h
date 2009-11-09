59f9dc952826606fc9e22dc40a879a9d1d122769
Erwin Waterlander <waterlan@xs4all.nl>
2009-11-09 22:34:31 +0000

    5.1.1-beta2

--- wcd/src/wcd.h
+++ wcd/src/wcd.h
@@ -82,8 +82,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #  define OP_DIR "."
 #endif /* ?MSDOS|VMS */
 
-#define VERSION      "5.1.1-beta1"
-#define VERSION_DATE "Oct 30 2009"
+#define VERSION      "5.1.1-beta2"
+#define VERSION_DATE "Nov 9 2009"
 
 
 /* Function prototypes */
