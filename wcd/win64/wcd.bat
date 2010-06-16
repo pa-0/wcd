84e5150a3c1d939b7567dfd68892590af5b68a5e
Erwin Waterlander <waterlan@xs4all.nl>
2010-06-16 21:33:16 +0000

    win64 version

---
+++ wcd/win64/wcd.bat
@@ -0,0 +1,20 @@
+@echo off
+
+set PDC_RESTORE_SCREEN=1
+rem set WCDHOME=%HOMEDRIVE%%HOMEPATH%
+rem set LANG=nl_NL
+rem set LANGUAGE=nl
+rem set WCDLOCALEDIR=c:/Program Files/wcd/share/locale
+
+wcdwin32.exe %*
+
+IF DEFINED WCDHOME (
+ %WCDHOME%\wcdgo.bat
+) ELSE (
+  IF DEFINED HOME (
+    %HOME%\wcdgo.bat
+  ) ELSE (
+    c:\wcdgo.bat
+  )
+)
+
