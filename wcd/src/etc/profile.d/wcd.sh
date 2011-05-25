80e226fd365a5cd25d94ab30c70a800007d80621
Erwin Waterlander <waterlan@xs4all.nl>
2011-05-25 16:28:28 +0000

    Update licenses; update shell startup scripts.

--- wcd/src/etc/profile.d/wcd.sh
+++ wcd/src/etc/profile.d/wcd.sh
@@ -1,28 +1,51 @@
+#   Copyright
 #
-#  Copyright (C) 2004-2011 Jari Aalto; Licensed under GPL v2 or later
+#       Copyright (C) 2011      Erwin Waterlander
+#       Copyright (C) 2009-2011 Jari Aalto
 #
-#  Define a shell alias for wcd(1), which itself change
-#  directories, so it must be done in a shell alias.
+#   License
 #
-#  This file is in /etc/profile.d/ and it should be sourced from
-#  a shell startup file: ~/.<shell>rc
+#       This program is free software; you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation; either version 2 of the License, or
+#       (at your option) any later version.
+#
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#       GNU General Public License for more details.
+#
+#       You should have received a copy of the GNU General Public License
+#       along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+#   Description
+#
+#       Define a shell alias for wcd(1), which itself change
+#       directories, so it must be done in a shell alias.
+#
+#       This file is in /etc/profile.d/ and it should be sourced from
+#       a shell startup file: ~/.<shell>rc
 
-#export WCDHOME="$HOME/.wcd"
+
+##if [[ -z $WCDHOME ]]
+##then
+##    export WCDHOME="$HOME/.wcd"
+##fi
 
 if [ -x "BINDIR/wcd" ]
 then
 
     wcd ()
     {
-	go="${WCDHOME:-${HOME}}/bin/wcd.go"
+        go="${WCDHOME:-${HOME}}/bin/wcd.go"
 
-	rm -f "$go" 2> /dev/null
+        rm -f "$go" 2> /dev/null
 
-	BINDIR/wcd "$@"
+        BINDIR/wcd "$@"
 
-	[ -f "$go" ] && . "$go"
+        [ -f "$go" ] && . "$go"
 
-	unset go
+        unset go
     }
 
 fi
