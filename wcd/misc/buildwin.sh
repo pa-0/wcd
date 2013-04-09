225edff0f7b5ecd787c88179f88ac18f525363bb
Erwin Waterlander <waterlan@xs4all.nl>
2013-04-09 17:54:08 +0000

    script to build all windows versions

---
+++ wcd/misc/buildwin.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+cd ../win32
+make mostlyclean all strip install dist UNINORM=1
+make mostlyclean all strip install dist
+cd ../win32psh
+make mostlyclean all strip install dist UNINORM=1
+make mostlyclean all strip install dist
+cd ../win32zsh
+make mostlyclean all strip install dist
+cd ../win64
+make mostlyclean all strip install dist UNINORM=1
+make mostlyclean all strip install dist
+cd ../win64psh
+make mostlyclean all strip install dist UNINORM=1
+make mostlyclean all strip install dist
+
