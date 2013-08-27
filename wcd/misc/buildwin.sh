3897914f106ad629a0c6d2012ae1bf2dedeb07e1
Erwin Waterlander <waterlan@xs4all.nl>
2013-08-27 21:03:49 +0000

    build win32 without NLS

--- wcd/misc/buildwin.sh
+++ wcd/misc/buildwin.sh
@@ -6,9 +6,11 @@ set -x
 cd ../win32
 make mostlyclean all strip install dist UNINORM=1
 make mostlyclean all strip install dist
+make mostlyclean all strip install dist ENABLE_NLS=
 cd ../win32psh
 make mostlyclean all strip install dist UNINORM=1
 make mostlyclean all strip install dist
+make mostlyclean all strip install dist ENABLE_NLS=
 cd ../win32zsh
 make mostlyclean all strip install dist
 cd ../win64
