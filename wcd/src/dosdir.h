b13412ce488f83d34eebff7149497642338671e6
Erwin Waterlander <waterlan@xs4all.nl>
2014-05-21 22:42:44 +0000

    update comment

--- wcd/src/dosdir.h
+++ wcd/src/dosdir.h
@@ -38,7 +38,7 @@
 #ifdef _MSC_VER
 typedef unsigned int mode_t;
 #endif
-/* intptr_t is new since C99. An unsigned integer type capable of holding a pointer.
+/* intptr_t is new since C99. An integer type capable of holding a pointer.
    When you use an older C standard your compiler may still support intptr_t.
    Older compilers may not support it. Wcd built with Microsoft Visual C++ 
    64 bit crashes if we don't use intptr_t */
