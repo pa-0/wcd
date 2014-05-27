10971b8eb5cfb0100730125674df2d6832f25528
Erwin Waterlander <waterlan@xs4all.nl>
2014-05-27 21:07:56 +0000

    build win64zsh

--- wcd/misc/bldwin64.sh
+++ wcd/misc/bldwin64.sh
@@ -13,4 +13,9 @@ make mostlyclean all strip install dist ENABLE_NLS=  UCS=
 make mostlyclean all strip install dist ENABLE_NLS=1 UCS=
 make mostlyclean all strip install dist ENABLE_NLS=  UNINORM=1
 make mostlyclean all strip install dist ENABLE_NLS=1 UNINORM=1
+cd ../win64zsh
+make mostlyclean all strip install dist ENABLE_NLS=  UCS=
+make mostlyclean all strip install dist ENABLE_NLS=1 UCS=
+make mostlyclean all strip install dist ENABLE_NLS=  UNINORM=1
+make mostlyclean all strip install dist ENABLE_NLS=1 UNINORM=1
 
