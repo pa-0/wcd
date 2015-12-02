8bd961c29bbf985cb0739e073f30135c44d4daa7
Erwin Waterlander <waterlan@xs4all.nl>
2015-12-02 21:47:21 +0000

    * version 5.3.1 * Unicode by default enabled for Visual C and Watcom.

--- wcd/misc/bldwin32.sh
+++ wcd/misc/bldwin32.sh
@@ -16,5 +16,7 @@ make mostlyclean all strip install dist ENABLE_NLS=1 UNINORM=1
 cd ../win32zsh
 make mostlyclean all strip install dist ENABLE_NLS=  UCS=
 make mostlyclean all strip install dist ENABLE_NLS=1 UCS=
+make mostlyclean all strip install dist ENABLE_NLS=  UNINORM=1
+make mostlyclean all strip install dist ENABLE_NLS=1 UNINORM=1
 
 
