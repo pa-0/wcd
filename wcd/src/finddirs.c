39a50d386bc377c963e83256d1c36834e3459f80
Erwin Waterlander <waterlan@xs4all.nl>
2017-01-27 00:20:28 +0100

    * finddirs.c: Don't use DOSDIR for disc scanning on Windows.               Using Windows API gives 4 times faster disc scanning.

--- wcd/src/finddirs.c
+++ wcd/src/finddirs.c
@@ -16,6 +16,13 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
+#ifdef WCD_UNICODE
+#  define UNICODE
+#  define _UNICODE
+#endif
+#ifdef _WIN32
+#  include <windows.h>
+#endif
 #include <string.h>
 #include "dosdir.h"
 #include "wcddir.h"
@@ -23,6 +30,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #include "wfixpath.h"
 #include "nameset.h"
 #include "config.h"
+#include "display.h"
 
 const wcd_char *default_mask = ALL_FILES_MASK;
 
@@ -96,13 +104,6 @@ int q_remove(TDirList *list,char *s)
    return 1;         /* okay */
 }
 
-/********************************************************************
- *
- *                    rmTree(dir)
- *
- * Recursively delete directory: *dir
- *
- ********************************************************************/
 /*
 The 32 bit dos versions are compiled with DJGPP and do not use DOSDIR. DJGPP is
 a mix of DOS/Unix (both 'MSDOS' and 'unix' are defined).  DOSDIR's
@@ -113,7 +114,19 @@ Also using DOSDIR's dd_findfirst/dd_findnext for DOS is very slow when it is
 compiled with DJGPP.  It is about a factor 35 slower than using DJGPP's
 findfirst/findnext.  Probably due to a slow stat() function in dd_initstruct().
 Using DOSDIR in combination with DJGPP would make scanning the disk very slow.
+
+The Windows version of wcd don't use DOSDIR anymore. Using the Windows API functions results
+in about 4 times faster disc scanning. EW Jan 2017.
+
 */
+
+/********************************************************************
+ *
+ *                    rmTree(dir)
+ *
+ * Recursively delete directory: *dir
+ *
+ ********************************************************************/
 #ifdef __DJGPP__
 #  define WCD_FB_NAME  fb.ff_name
 #  define WCD_FB_MODE  fb.ff_attrib
@@ -200,9 +213,90 @@ void rmTree(char *dir)
  *  finddirs(char *dir, size_t *offset, FILE *outfile, int *use_HOME, int quiet)
  *
  ********************************************************************/
-/*
- * See comment above function rmTree().
- */
+#if defined(_WIN32) && !defined(__CYGWIN__)
+
+#ifdef WCD_UNICODE
+#  define WCD_FINDFIRSTFILE FindFirstFileW
+#  define WCD_FINDNEXTFILE  FindNextFileW
+#else
+#  define WCD_FINDFIRSTFILE FindFirstFileA
+#  define WCD_FINDNEXTFILE  FindNextFileA
+#endif
+void finddirs(char *dir, size_t *offset, FILE *outfile, int *use_HOME, nameset exclude, int quiet)
+{
+   char curdir[DD_MAXPATH];
+   char nextdir[DD_MAXPATH];
+   char *tmp_ptr ;
+   size_t len ;
+   WIN32_FIND_DATA FindFileData;
+   HANDLE hFind;
+
+   if (dir)
+   {
+      if (wcd_chdir(dir,quiet)) return; /* Go to the dir, else return */
+   }
+   else
+     return ;  /* dir == NULL */
+
+
+   if (wcd_getcwd(curdir, sizeof(curdir)) == NULL)
+   {
+      print_error(_("finddirs(): can't determine path in directory %s\n"),dir);
+      print_error(_("path probably too long.\n"));
+      wcd_chdir(DIR_PARENT,1); /* go to parent directory */
+      return;
+   };
+
+#ifdef _WCD_DOSFS
+   wcd_fixpath(curdir,sizeof(curdir));
+   rmDriveLetter(curdir,use_HOME);
+#endif
+
+   if (pathInNameset(curdir,exclude) != (size_t)-1)
+   {
+      wcd_chdir(DIR_PARENT,1); /* go to parent directory */
+      return;
+   }
+
+   len = strlen(curdir);
+
+   if(*offset < len)
+     tmp_ptr = curdir + *offset ;
+   else
+     tmp_ptr = curdir + len;   /* tmp_ptr points to ending '\0' of curdir */
+
+   if (wcd_fprintf(outfile,"%s\n", tmp_ptr) < 0)
+      return;  /* Quit when we can't write path to disk */
+
+   hFind = WCD_FINDFIRSTFILE(ALL_FILES_MASK, &FindFileData);
+   while (hFind != INVALID_HANDLE_VALUE)
+   {
+      if (FindFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
+         char directory[DD_MAXPATH];
+#ifdef WCD_UNICODE
+         wcstoutf8(directory, FindFileData.cFileName, sizeof(directory));
+#else
+         wcd_strncpy(directory,FindFileData.cFileName,sizeof(directory));
+#endif
+         if (!SpecialDir(directory)) {
+            wcd_strncpy(nextdir,curdir,sizeof(nextdir));
+            wcd_strncat(nextdir,"/",sizeof(nextdir));
+            wcd_strncat(nextdir,directory,sizeof(nextdir));
+            if(wcd_islink(nextdir,quiet))
+               wcd_fprintf(outfile,"%s\n", nextdir);
+            else
+               finddirs(nextdir,offset, outfile, use_HOME, exclude, 1);
+         }
+
+      }
+      if (!WCD_FINDNEXTFILE(hFind, &FindFileData)) {
+          FindClose(hFind);
+          hFind = INVALID_HANDLE_VALUE;
+      }
+
+   }
+}
+#else
 void finddirs(char *dir, size_t *offset, FILE *outfile, int *use_HOME, nameset exclude, int quiet)
 {
 #ifdef __DJGPP__
@@ -212,7 +306,7 @@ void finddirs(char *dir, size_t *offset, FILE *outfile, int *use_HOME, nameset e
    static dd_ffblk fb;           /* file block structure */
    wcd_intptr_t rc;              /* error code */
 #endif
-   static char tmp[DD_MAXPATH];  /* tmp string buffer */
+   static char curdir[DD_MAXPATH];  /* curdir string buffer */
    size_t len ;
    TDirList list;                /* directory queue */
    char *tmp_ptr ;
@@ -225,7 +319,7 @@ void finddirs(char *dir, size_t *offset, FILE *outfile, int *use_HOME, nameset e
      return ;  /* dir == NULL */
 
 
-   if (wcd_getcwd(tmp, sizeof(tmp)) == NULL)
+   if (wcd_getcwd(curdir, sizeof(curdir)) == NULL)
    {
       print_error(_("finddirs(): can't determine path in directory %s\n"),dir);
       print_error(_("path probably too long.\n"));
@@ -234,22 +328,22 @@ void finddirs(char *dir, size_t *offset, FILE *outfile, int *use_HOME, nameset e
    };
 
 #ifdef _WCD_DOSFS
-   wcd_fixpath(tmp,sizeof(tmp));
-   rmDriveLetter(tmp,use_HOME);
+   wcd_fixpath(curdir,sizeof(curdir));
+   rmDriveLetter(curdir,use_HOME);
 #endif
 
-   if (pathInNameset(tmp,exclude) != (size_t)-1)
+   if (pathInNameset(curdir,exclude) != (size_t)-1)
    {
       wcd_chdir(DIR_PARENT,1); /* go to parent directory */
       return;
    }
 
-   len = strlen(tmp);
+   len = strlen(curdir);
 
    if(*offset < len)
-     tmp_ptr = tmp + *offset ;
+     tmp_ptr = curdir + *offset ;
    else
-     tmp_ptr = tmp + len;   /* tmp_ptr points to ending '\0' of tmp */
+     tmp_ptr = curdir + len;   /* tmp_ptr points to ending '\0' of curdir */
 
    if (wcd_fprintf(outfile,"%s\n", tmp_ptr) < 0)
       return;  /* Quit when we can't write path to disk */
@@ -310,9 +404,10 @@ void finddirs(char *dir, size_t *offset, FILE *outfile, int *use_HOME, nameset e
    } /* while !rc */
 
    /* recursively parse subdirectories (if any) (quiet) */
-   while (q_remove(&list, tmp))
-      finddirs(tmp,offset, outfile, use_HOME, exclude, 1);
+   while (q_remove(&list, curdir))
+      finddirs(curdir,offset, outfile, use_HOME, exclude, 1);
 
    /* Quiet, because on OS/2 changing to .. from a disk root dir gives an error. */
    wcd_chdir(DIR_PARENT,1); /* go to parent directory */
 }
+#endif
