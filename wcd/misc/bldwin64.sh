3a847c48d125b96e9180795be562704c7c5e69e5
Erwin Waterlander <waterlan@xs4all.nl>
2018-04-11 22:55:58 +0200

    Windows build scripts: build only English,no Unicode and International with Unicode.

--- wcd/misc/bldwin64.sh
+++ wcd/misc/bldwin64.sh
@@ -5,17 +5,10 @@ set -x
 
 cd ../win64
 make mostlyclean all strip install dist ENABLE_NLS=  UCS=
-make mostlyclean all strip install dist ENABLE_NLS=1 UCS=
-make mostlyclean all strip install dist ENABLE_NLS=  UNINORM=1
 make mostlyclean all strip install dist ENABLE_NLS=1 UNINORM=1
 cd ../win64psh
 make mostlyclean all strip install dist ENABLE_NLS=  UCS=
-make mostlyclean all strip install dist ENABLE_NLS=1 UCS=
-make mostlyclean all strip install dist ENABLE_NLS=  UNINORM=1
 make mostlyclean all strip install dist ENABLE_NLS=1 UNINORM=1
 cd ../win64zsh
 make mostlyclean all strip install dist ENABLE_NLS=  UCS=
-make mostlyclean all strip install dist ENABLE_NLS=1 UCS=
-make mostlyclean all strip install dist ENABLE_NLS=  UNINORM=1
 make mostlyclean all strip install dist ENABLE_NLS=1 UNINORM=1
-
